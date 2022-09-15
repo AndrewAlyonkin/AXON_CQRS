5957d33bf278932d3c529ebb5276601e889c95cb
AlenkinAndrew <oxqq@ya.ru>
2022-09-15 11:07:39 +0300

    init

---
+++ core/src/main/java/com/afalenkin/core/config/AxonConfig.java
@@ -0,0 +1,11 @@
+package com.afalenkin.core.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Alenkin Andrew
+ * oxqq@ya.ru
+ */
+@Configuration
+public class AxonConfig {
+}
---
+++ user-cmd-api/src/test/java/com/afalenkin/usercmdapi/UserCmdApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afalenkin.usercmdapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserCmdApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
