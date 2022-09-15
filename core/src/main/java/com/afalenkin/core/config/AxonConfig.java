56d0bc7f39f115a38bff640dd6d260fa3c634f87
AlenkinAndrew <oxqq@ya.ru>
2022-09-15 12:26:06 +0300

    axon config

--- core/src/main/java/com/afalenkin/core/config/AxonConfig.java
+++ core/src/main/java/com/afalenkin/core/config/AxonConfig.java
@@ -1,6 +1,24 @@
 package com.afalenkin.core.config;
 
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoClient;
+import org.axonframework.eventhandling.tokenstore.TokenStore;
+import org.axonframework.eventsourcing.eventstore.EmbeddedEventStore;
+import org.axonframework.eventsourcing.eventstore.EventStorageEngine;
+import org.axonframework.eventsourcing.eventstore.EventStore;
+import org.axonframework.extensions.mongo.DefaultMongoTemplate;
+import org.axonframework.extensions.mongo.MongoTemplate;
+import org.axonframework.extensions.mongo.eventsourcing.eventstore.MongoEventStorageEngine;
+import org.axonframework.extensions.mongo.eventsourcing.eventstore.MongoFactory;
+import org.axonframework.extensions.mongo.eventsourcing.eventstore.MongoSettingsFactory;
+import org.axonframework.extensions.mongo.eventsourcing.tokenstore.MongoTokenStore;
+import org.axonframework.serialization.Serializer;
+import org.axonframework.spring.config.AxonConfiguration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Collections;
 
 /**
  * @author Alenkin Andrew
@@ -8,4 +26,52 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class AxonConfig {
+    @Value("${spring.data.mongodb.host:127.0.0.1}")
+    private String mongoHost;
+
+    @Value("${spring.data.mongodb.port:27017}")
+    private int mongoPort;
+
+    @Value("${spring.data.mongodb.database:user}")
+    private String mongoDbName;
+
+    @Bean
+    public EventStorageEngine storageEngine(MongoClient mongoClient) {
+        return MongoEventStorageEngine.builder()
+                .mongoTemplate(DefaultMongoTemplate.builder().mongoDatabase(mongoClient).build())
+                .build();
+    }
+
+    @Bean
+    public EmbeddedEventStore eventStore(EventStorageEngine storageEngine, AxonConfiguration axonConfiguration) {
+        return EmbeddedEventStore.builder()
+                .storageEngine(storageEngine)
+                .messageMonitor(axonConfiguration.messageMonitor(EventStore.class, "eventStore"))
+                .build();
+    }
+
+    @Bean
+    public MongoClient mongoClient() {
+        MongoFactory mongoFactory = new MongoFactory();
+        MongoSettingsFactory settingsFactory = new MongoSettingsFactory();
+        settingsFactory.setMongoAddresses(Collections.singletonList(new ServerAddress(mongoHost, mongoPort)));
+        mongoFactory.setMongoClientSettings(settingsFactory.createMongoClientSettings());
+
+        return mongoFactory.createMongo();
+    }
+
+    @Bean
+    public MongoTemplate axonMongoTemplate() {
+        return DefaultMongoTemplate.builder()
+                .mongoDatabase(mongoClient(), mongoDbName)
+                .build();
+    }
+
+    @Bean
+    public TokenStore tokenStore(Serializer serializer) {
+        return MongoTokenStore.builder()
+                .mongoTemplate(axonMongoTemplate())
+                .serializer(serializer)
+                .build();
+    }
 }
--- query-api/src/main/java/com/afalenkin/queryapi/QueryApiApplication.java
+++ query-api/src/main/java/com/afalenkin/queryapi/QueryApiApplication.java
@@ -1,9 +1,12 @@
 package com.afalenkin.queryapi;
 
+import com.afalenkin.core.config.AxonConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(AxonConfig.class)
 public class QueryApiApplication {
 
     public static void main(String[] args) {
